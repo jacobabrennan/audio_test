a4a06ce88ea5ea76a442db74cfdce4aa3e14b763
Jacob A Brennan <jacobabrennan@gmail.com>
2020-08-17 22:44:57 -0400

    Begin replacing html rendering with canvas

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ import {
 } from './pattern_editor/index.js';
 import { setup as setupControls } from './controls/index.js';
 import { patternAdd } from './controls/pattern.js';
+import {
+    cell,
+    pattern,
+    CHANNELS_NUMBER,
+} from './processor.js';
 
 //-- Constants -----------------------------------
 const DOM_ID_CLIENT = 'client';
@@ -24,6 +29,25 @@ const DOM_ID_CLIENT = 'client';
 // Set up buttons
 
 //------------------------------------------------
+
+export function randomPattern(rows) {
+    const patternRandom = pattern(rows, CHANNELS_NUMBER);
+    for(let I = 0; I < rows; I++) {
+        const note = Math.floor(Math.random()*24)+24;
+        patternRandom[I*CHANNELS_NUMBER] = cell(note,0,32,0);
+        if(!(I%2)) {
+            patternRandom[I*CHANNELS_NUMBER+1] = cell(note-7,0,32,0);
+            patternRandom[I*CHANNELS_NUMBER+4] = cell(1,1,63,0);
+        } else {
+            // patternRandom[I*CHANNELS_NUMBER] = cell(MASK_CELL_NOTE_STOP,0,null,0);
+        }
+        if(!(I%4)) {
+            patternRandom[(I*CHANNELS_NUMBER)+4] = cell(5,2,63,0);
+            patternRandom[(I*CHANNELS_NUMBER)+3] = cell(28,2,63,0);
+        }
+    }
+    return patternRandom;
+}
 // import {
 //     pattern,
 //     cell,
--- pattern_editor_old/cell.js
+++ pattern_editor_old/cell.js
@@ -114,3 +114,43 @@ export default class Cell {
     }
     validateEffects() {}
 }
+
+//-- Utilities -----------------------------------
+const noteLetters = ['A','Bb','B','C','C#','D','Eb','E','F','F#','G','Ab'];
+function noteFormatName(note) {
+    let letter = note[0].toUpperCase();
+    let octave = 2;
+    if(note.length === 1) { // 'b' => 'B 4'
+        return letter+' '+octave;
+    }
+    if(note.length === 3) { // 'bb2' => 'Bb2'
+        return letter+note[1]+note[2];
+    }
+    if(note[1] === '#' || note[1] === 'b') { // 'bb' => 'Bb4'
+        return letter+note[1]+octave;
+    }
+    return letter+' '+note[1]; // 'b4' => 'B 4'
+}
+function noteNameToNumber(note) {
+    let letter = note[0];
+    if(note[1] !== ' ') {
+        letter = note[0]+note[1];
+    }
+    let noteOffset = noteLetters.indexOf(letter);
+    let octave = parseInt(note[2]);
+    if(noteOffset > 2) { octave--}
+    if(noteOffset === -1) { return false;}
+    noteOffset += octave*12;
+    if(!Number.isFinite(noteOffset)) { return false;}
+    if(noteOffset > (Math.pow(2, MASK_CELL_NOTE_WIDTH)-1)) { return false;}
+    if(noteOffset < 0) { return false;}
+    return noteOffset;
+}
+function noteNumberToName(note) {
+    let letter = noteLetters[note%12];
+    let octave = Math.floor(note/12);
+    if(note%12 > 2) {
+        octave++;
+    }
+    return letter.padEnd(2,' ') + octave.toString();
+}
