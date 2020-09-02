3f77a8f80b7aff556e00759f9d3b28dc9ff26145
Jacob A Brennan <jacobabrennan@gmail.com>
2020-09-01 20:13:00 -0400

    Factor music-editor out of Vue mounting / font setup

--- client.js
+++ client.js
@@ -16,38 +16,23 @@ import {
     VOLUME_MAX,
 } from './processor.js';
 import {
-    contextConfigure,
-    CHAR_HEART,
     DISPLAY_HEIGHT_DEFAULT,
-    DOM_STYLE_DYNAMIC,
-    // EDITOR_PANE_PATTERN,
-    FONT_SIZE,
 } from './utilities.js';
 import { DISPLAY_PIXEL_WIDTH } from './editor_pattern/canvas.js';
 import { EVENT_ADJUST } from './controls/adjuster.js';
-// import { setup as setupEditorPattern } from './editor_pattern/index.js';
-// import { setup as setupEditorInstrument } from './editor_instrument/index.js';
-// import { setup as setupControls } from './pane/pane_control.js';
-// import {
-//     setup as setupEditor,
-//     paneSelect,
-// } from './pane/pane_editor.js';
 
 //-- Constants -----------------------------------
 const DOM_ID_CLIENT = 'client';
 
-//-- Setup ---------------------------------------
-export async function setup() {
-    // Load custom font
-    await loadFont();
-    // Create DOM container
-    let client = document.createElement('div');
-    document.body.append(client);
-    client.outerHTML = (`
+//------------------------------------------------
+Vue.component('song-editor', {
+    template: `
         <div id=${DOM_ID_CLIENT}>
             <div id="editor" style="width:${DISPLAY_PIXEL_WIDTH}">
-                <editor-pattern :pattern="patternCurrent" :height="${DISPLAY_HEIGHT_DEFAULT}">
-                </editor-pattern>
+                <editor-pattern
+                    :pattern="patternCurrent"
+                    :height="${DISPLAY_HEIGHT_DEFAULT}"
+                />
             </div>
             <client-controls id="controls">
                 <value-adjuster
@@ -59,11 +44,9 @@ export async function setup() {
                 ></value-adjuster>
             </client-controls>
         </div>
-    `);
-    // Create Vue Instance
-    new Vue({
-        el: `#${DOM_ID_CLIENT}`,
-        data: {
+    `,
+    data: function () {
+        return {
             volume: 4,
             beatsPerSecond: BPS_DEFAULT,
             ticksPer: TPB_DEFAULT,
@@ -73,70 +56,17 @@ export async function setup() {
             ],
             instrumentCurrentIndex: 0,
             instruments: [],
+        };
+    },
+    computed: {
+        patternCurrent() {
+            return this.patterns[this.patternCurrentIndex];
+        }
+    },
+    methods: {
+        handleAdjustVolume(valueNew) {
+            valueNew = Math.max(0, Math.min(VOLUME_MAX, valueNew));
+            this.volume = valueNew;
         },
-        computed: {
-            patternCurrent() {
-                return this.patterns[this.patternCurrentIndex];
-            }
-        },
-        methods: {
-            handleAdjustVolume(valueNew) {
-                valueNew = Math.max(0, Math.min(VOLUME_MAX, valueNew));
-                this.volume = valueNew;
-            },
-        },
-    });
-}
-
-
-//== Font Load Checker =========================================================
-
-/* This font loader is necessary due to Chrome's refusal to load fonts until
-after an attempt is made to use them. This is too late for a call to the canvas
-2D drawing functions. Until FontFace becomes an offical stanard, a kludge like
-this will remain necessary. Switch to Firefox, where this just works. */
-
-//-- Font Loader ---------------------------------
-async function loadFont() {
-    const elementStyle = document.createElement('style');
-    elementStyle.innerText = DOM_STYLE_DYNAMIC;
-    document.head.appendChild(elementStyle);
-    const canvas = document.createElement('canvas');
-    canvas.width = FONT_SIZE;
-    canvas.height = FONT_SIZE;
-    const context = canvas.getContext('2d');
-    contextConfigure(context);
-    let runningTotal = 0;
-    const timeoutFont = 1000; // Give chrome a second to attempt to load the font.
-    return new Promise((resolve, reject) => {
-        const interval = setInterval(() => {
-            const success = checkFontStatus(context);
-            if(success) {
-                clearInterval(interval);
-                resolve();
-            }
-            runningTotal += 10;
-            if(runningTotal > timeoutFont) {
-                clearInterval(interval);
-                reject();
-            }
-        }, 10);
-    });
-}
-function checkFontStatus(context) {
-    context.fillStyle = '#000';
-    context.fillRect(0,0,FONT_SIZE,FONT_SIZE);
-    context.fillStyle = '#f00';
-    context.fillText(CHAR_HEART, 0, FONT_SIZE);
-    const imageData = context.getImageData(0,0,FONT_SIZE,FONT_SIZE).data;
-    const posY = 8;
-    const colorChannels = 4;
-    let success = true;
-    for(let posX = 4; posX < FONT_SIZE-4; posX++) {
-        const indexRedChannel = ((posY*FONT_SIZE)+posX)*colorChannels;
-        const valueRed = imageData[indexRedChannel];
-        if(valueRed === 255) { continue;}
-        success = false;
-    }
-    return success;
-}
+    },
+});
--- index.js
+++ index.js
@@ -3,7 +3,76 @@
 //==============================================================================
 
 //-- Dependencies --------------------------------
-import { setup as setupClient } from './client.js';
+import Vue from './libraries/vue.esm.browser.js';
+import './client.js';
+import {
+    contextConfigure,
+    CHAR_HEART,
+    DOM_STYLE_DYNAMIC,
+    FONT_SIZE,
+} from './utilities.js';
 
 //-- Initialize Project --------------------------
-setupClient();
+(async function () {
+    // Load custom font
+    await loadFont();
+    // Create DOM container
+    let client = document.createElement('song-editor');
+    document.body.append(client);
+    new Vue({
+        el: client,
+    });
+})();
+
+
+//== Font Load Checker =========================================================
+
+/* This font loader is necessary due to Chrome's refusal to load fonts until
+after an attempt is made to use them. This is too late for a call to the canvas
+2D drawing functions. Until FontFace becomes an offical stanard, a kludge like
+this will remain necessary. Switch to Firefox, where this just works. */
+
+//-- Font Loader ---------------------------------
+async function loadFont() {
+    const elementStyle = document.createElement('style');
+    elementStyle.innerText = DOM_STYLE_DYNAMIC;
+    document.head.appendChild(elementStyle);
+    const canvas = document.createElement('canvas');
+    canvas.width = FONT_SIZE;
+    canvas.height = FONT_SIZE;
+    const context = canvas.getContext('2d');
+    contextConfigure(context);
+    let runningTotal = 0;
+    const timeoutFont = 1000; // Give chrome a second to attempt to load the font.
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+            const success = checkFontStatus(context);
+            if(success) {
+                clearInterval(interval);
+                resolve();
+            }
+            runningTotal += 10;
+            if(runningTotal > timeoutFont) {
+                clearInterval(interval);
+                reject();
+            }
+        }, 10);
+    });
+}
+function checkFontStatus(context) {
+    context.fillStyle = '#000';
+    context.fillRect(0,0,FONT_SIZE,FONT_SIZE);
+    context.fillStyle = '#f00';
+    context.fillText(CHAR_HEART, 0, FONT_SIZE);
+    const imageData = context.getImageData(0,0,FONT_SIZE,FONT_SIZE).data;
+    const posY = 8;
+    const colorChannels = 4;
+    let success = true;
+    for(let posX = 4; posX < FONT_SIZE-4; posX++) {
+        const indexRedChannel = ((posY*FONT_SIZE)+posX)*colorChannels;
+        const valueRed = imageData[indexRedChannel];
+        if(valueRed === 255) { continue;}
+        success = false;
+    }
+    return success;
+}
